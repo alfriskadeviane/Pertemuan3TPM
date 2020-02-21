67b164f68694f87c187c361f9ad9161e4671eb3c
alfriskadeviane <alfriskadeviane@gmail.com>
2020-02-21 11:48:10 +0700

    first commit

---
+++ app/src/main/java/com/example/pertemuan3rv/Main2Activity.java
@@ -0,0 +1,30 @@
+package com.example.pertemuan3rv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class Main2Activity extends AppCompatActivity {
+    private TextView tvNama,tvDetail;
+    private ImageView ivPahlawan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        tvNama= findViewById(R.id.tvNama);
+        tvDetail= findViewById(R.id.tvDetail);
+
+        Bundle bundle= getIntent().getExtras();
+        String nama= bundle.getString("nama");
+        String detail= bundle.getString("detail");
+
+
+        tvNama.setText(nama);
+        tvDetail.setText(detail);
+    }
+}
